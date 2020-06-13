71388b351b342d7b773468073b952805f2309645
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2020-06-14 00:31:30 +0530

    Update ReverseString.py

--- Python/String_Problems/ReverseString.py
+++ Python/String_Problems/ReverseString.py
@@ -1,8 +1,13 @@
 def reverse(str1):
+  #Check Input
+  if(len(str1)==0 or len(str1)==1):
+    return "Not Eligible for Reverse"
+
   list1=[]
   str2=""
-  for i in range(len(str1)-1,-1,-1):
+  length=len(str1)
+  for i in range(length-1,-1,-1):
     list1.append(str1[i])
   return str2.join(list1)
-  
+
 print(reverse("My name is anshUMan"))
