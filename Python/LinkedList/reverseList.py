8c65beb6b47a60e7bc5e35896c58281c65ae26f1
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2020-06-20 03:22:05 +0530

    Update reverseList.py

--- Python/LinkedList/reverseList.py
+++ Python/LinkedList/reverseList.py
@@ -1,3 +1,12 @@
+'''
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+'''
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
