f08c12d70bcc90d1a5483d6ba2cd61224ee47c3f
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2020-06-20 03:21:28 +0530

    Update isPalindrome.py

--- Python/LinkedList/isPalindrome.py
+++ Python/LinkedList/isPalindrome.py
@@ -1,3 +1,16 @@
+'''
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+'''
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
