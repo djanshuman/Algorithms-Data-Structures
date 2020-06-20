915b3206c3646e15814ab598a9aa24085edd37ea
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2020-06-20 22:04:24 +0530

    Create removeNthFromEnd.py

---
+++ Python/LinkedList/removeNthFromEnd.py
@@ -0,0 +1,68 @@
+'''
+Given a linked list, remove the n-th node from the end of list and return its head.
+
+Example:
+
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        '''To check Empty node'''
+        if(head is None):
+            return False
+        
+        first=head  
+        tail=head.next
+        len_of_list=1
+        pos=0
+        temp=None
+        
+        '''To check if Input is single element'''
+        if(tail is None):
+            first=None
+            head=first
+            return head
+          
+        '''To calculate the length of LL and store in len_of_list'''
+        while(tail.next != None): 
+            print(1)
+            tail=tail.next
+            len_of_list+=1
+    
+        ''' To know until which node our First pointer should travel for deletion. Note length of list will always be (len-1) as we are 
+        incrementing len of list inside while loop and coming out of loop when tail.next is None so not considering last node'''
+        
+        first_to_traverse=(len_of_list-n)
+        
+        '''If first element deletion is targeted '''
+        if(first_to_traverse < 0):
+            temp=first.next
+            first=temp
+            head=first
+            
+        '''We traverse our first pointer to the previous node of to be deleted node and increment pos value '''
+        while(pos <first_to_traverse and first_to_traverse!=0):
+            first=first.next
+            pos+=1
+            
+        '''Deletion happens and pointer shifting'''
+        if(pos==first_to_traverse or first_to_traverse==0):
+            temp=first.next.next
+            first.next=temp
+            
+        return head
+
+                
