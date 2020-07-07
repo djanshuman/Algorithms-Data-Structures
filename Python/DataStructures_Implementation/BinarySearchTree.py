2dca12635776590d03cfce527c4b4e34bfd20c73
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2020-07-07 23:05:18 +0530

    Update BinarySearchTree.py

--- Python/DataStructures_Implementation/BinarySearchTree.py
+++ Python/DataStructures_Implementation/BinarySearchTree.py
@@ -1,132 +1,106 @@
 '''
-Created on 05-Jul-2020
+Created on 07-Jul-2020
 
-@author: dibyajyoti
+@author: dibyajyoti.nayak
 '''
 ''' Functionalities : Insert, Delete, Lookup, Maxvalue, minValue'''
 
-
 class Node:
-    def __init__(self,data):
-        self.data=data
+    
+    def __init__(self,value):
+        self.data=value
         self.left=None
         self.right=None
         
-class BinarySearchTree:
-    
-    def __init__(self):
-        self.root=None
-        
-        
-    def insert(self,value):
-        node=Node(value)
-        
-        if(self.root==None):
-            self.root=node
-             
-        else:
-            currValue=self.root
-            while(True):
-                if(value < currValue.data):
-                    #left
-                    if(currValue.left==None):
-                        currValue.left=node
-                        return
-                    else:
-                        currValue=currValue.left
-                
-                else:
-                    #right
-                    if(currValue.right==None):
-                        currValue.right=node
-                        return
-                    else:
-                        currValue=currValue.right
-                        
-    def lookup(self,value):
+def minvalue(node):
+    currvalue=node
+    while(currvalue.left is not None):
+        currvalue=currvalue.left
+    return currvalue
 
-        if(self.root==None):
-            return False
-        else:
-            currvalue=self.root
-            while(currvalue):
-                if(value < currvalue.data):
-                    currvalue=currvalue.left    
-                    
-                elif(value > currvalue.data):
-                    currvalue=currvalue.right
-                    
-                elif(currvalue.data==value):
-                    return True           
-            return False
-           
-    ''' In-Order traversal with a helper method''' 
-                 
-    def print_tree(self):
-        if(self.root!=None):
-            self.print_logic(self.root)
-            
-    def print_logic(self,curr_node):
-        if(curr_node!=None):
-            self.print_logic(curr_node.left)
-            print(str(curr_node.data))
-            self.print_logic(curr_node.right)
-            
-    ''' Find max and min value of BST'''
-            
-    def minValue(self,node):
-        currValue=node
-        while(currValue.left!=None):
-            currValue=currValue.left
-        return  currValue
+def maxvalue(node):
+    currvalue=node
+    while(currvalue is not None):
+        currvalue=currvalue.right
+    return currvalue
+      
+def insert(node,value):
     
-    def maxValue(self,root):
-        node=root
-        while(node.right!=None):
-            node=node.right
-        return node
+    if node is None:
+        return Node(value)
     
-    def find_max_min_Value(self):
+    if(value > node.data):
+        node.right=insert(node.right,value)
+    elif(value < node.data):
+        node.left=insert(node.left,value)
         
-        if(self.root==None):
-            return False
+    return node
+    
+def delete(node,value):
+    
+    if(node is None):
+        return False
+    
+    elif(value > node.data):
+        node.right=delete(node.right,value)
+    elif(value < node.data):
+        node.left=delete(node.left,value)
+    else:
+        if(node.left is None):
+            temp=node.right
+            node=None
+            return temp
+        elif(node.right is None):
+            temp=node.left
+            node=None
+            return temp
         else:
-            max_val=self.maxValue(self.root)
-            min_val=self.minValue(self.root)
             
-        print(max_val,min_val)
+            temp=minvalue(node.right)
+            node.data=temp.data
+            node.right=delete(node.right,temp.data)
+    return node
+
+def inorder(node):
+    if(node is not None):
+        inorder(node.left)
+        print(node.data),
+        inorder(node.right)
         
-    def delete(self,root,key):
-        if(root==None):
-            return False
-        
-        if(key > root.data):
-            root.right=self.delete(root.right, key)
-        elif(key < root.data):
-            root.left=self.delete(root.left, key)
-        else:
-            
-            if(root.left is None):
-                temp=root.right
-                root=None
-                return temp
-            elif(root.right is None):
-                temp=root.left
-                root=None
-                return temp
-            temp=self.minValue(root.right)
-            root.data=temp.data
-            root.right=self.delete(root.right, key)
-        return root
-        
-mybst=BinarySearchTree()
-mybst.insert(9)
-mybst.insert(4)
-mybst.insert(20)
-mybst.insert(3)
-mybst.insert(6)
-mybst.insert(15)
-mybst.insert(30)
-print(mybst.lookup(3))
-mybst.print_tree()
-#mybst.find_max_min_Value()
+def lookup(node,value):
+    if node is None:
+        return False
+    currvalue=node
+    while(currvalue):
+        if(value > currvalue.data):
+            currvalue=currvalue.right
+        elif(value < currvalue.data):
+            currvalue=currvalue.left
+        elif(currvalue.data == value):
+            return True
+    return False
+
+
+root = None
+root = insert(root, 50)
+root = insert(root, 30)  
+root = insert(root, 20) 
+root = insert(root, 40) 
+root = insert(root, 70) 
+root = insert(root, 60) 
+root = insert(root, 80) 
+root = insert(root, 100)
+print("\nInorder traversal of tree")
+inorder(root)
+print("\nDoing Look up on tree")
+print(lookup(root, 70))
+print("Deleting 70 node from the tree")
+delete(root,70)
+inorder(root)
+print("\nDeleting 50 node from the tree")
+delete(root,50)
+inorder(root)
+print("\nDeleting 30 node from the tree")
+delete(root,30)
+inorder(root)
+
