9aff40ab2141da4f3965bbf023861bc2c3dce919
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2020-07-19 22:45:15 +0530

    Create Breadthfirstsearch_forGraph.py
    #Console Output for 3 pattern
    
    Printing Graphs Vertex and Edges:
    A : ['B', 'C']
    B : ['A', 'D', 'E']
    C : ['A', 'D']
    D : ['B', 'C', 'E']
    E : ['B', 'D']
    
    Printing Graphs in BreadFirstSearch order :
    ({'A': 0, 'B': 1, 'C': 1, 'D': 2, 'E': 2}, {'A': None, 'B': 'A', 'C': 'A', 'D': 'B', 'E': 'B'})
    
    
    Printing Graphs Vertex and Edges:
    0 : [1, 2, 3]
    1 : [4, 5]
    2 : [6, 7]
    3 : [7]
    4 : [None]
    5 : [None]
    6 : [None]
    7 : [None]
    
    Printing Graphs in BreadFirstSearch order :
    ({0: 0, 1: 1, 2: 1, 3: 1, 4: 2, 5: 2, 6: 2, 7: 2}, {0: None, 1: 0, 2: 0, 3: 0, 4: 1, 5: 1, 6: 2, 7: 2})
    
    Printing Graphs Vertex and Edges:
    0 : [1, 3, 8]
    1 : [0, 7]
    7 : [1, 2]
    2 : [3, 5, 7]
    3 : [0, 2, 4]
    4 : [3, 8]
    5 : [2, 6]
    8 : [0, 4]
    6 : [5]
    
    Printing Graphs in BreadFirstSearch order :
    ({0: 0, 1: 1, 3: 1, 8: 1, 7: 2, 2: 2, 4: 2, 5: 3, 6: 4}, {0: None, 1: 0, 3: 0, 8: 0, 7: 1, 2: 3, 4: 3, 5: 2, 6: 5})
    
    Process finished with exit code 0

---
+++ Python/DataStructures_Implementation/Breadthfirstsearch_forGraph.py
@@ -0,0 +1,127 @@
+'''
+Created on 19-Jul-2020
+
+@author: dibyajyoti
+
+The Breadh First Search Implementation along with Shortest Path and Parent checks done using dictionary
+self.level=dict() -> Stores the distance from the searched vertex to rest all vertex.
+self.parent=dict() -> Stores the parent of each vertex that can be traced and Shows the BFS traversal
+order.
+'''
+from collections import defaultdict
+class Breadthfirstsearch:
+
+    def __init__(self):
+        self.graph=defaultdict(list)
+        self.level=dict()
+        self.parent=dict()
+        self.queue=[]
+
+    def addedge(self,v,e):
+        self.graph[v].append(e)
+
+    def printGraph(self):
+        for key in self.graph.keys():
+            print(key,':',self.graph[key])
+
+    def search(self,vertex):
+        self.level={vertex:0}
+        self.parent={vertex:None}
+        self.queue.append(vertex)
+
+        while(self.queue):
+            v=self.queue.pop(0)
+            for e in self.graph[v]:
+                if e not in self.level and e!=None:
+                    self.queue.append(e)
+                    self.level[e]=self.level[v]+1
+                    self.parent[e]=v
+        return self.level,self.parent  # if interested to see only traversal order self.parent.keys()
+
+'''
+1. First Pattern
+       0
+    /  |  \ 
+    1  2  3
+    /\ |  |
+   4 5 6  7 
+   
+2. Second Pattern
+ 
+A : ['B', 'C']
+B : ['A', 'D', 'E']
+C : ['A', 'D']
+D : ['B', 'C', 'E']
+E : ['B', 'D']  
+
+3. Third Pattern
+   0 - 1 -7
+ / |    /
+8  3 - 2    6
+ \ |    \  /
+   4     5          
+'''
+
+bst=Breadthfirstsearch()
+bst.addedge('A','B')
+bst.addedge('A','C')
+bst.addedge('B','A')
+bst.addedge('B','D')
+bst.addedge('B','E')
+bst.addedge('C','A')
+bst.addedge('C','D')
+bst.addedge('D','B')
+bst.addedge('D','C')
+bst.addedge('D','E')
+bst.addedge('E','B')
+bst.addedge('E','D')
+print("\nPrinting Graphs Vertex and Edges:")
+bst.printGraph()
+print("\nPrinting Graphs in BreadFirstSearch order :")
+print(bst.search('A'))
+print("\n")
+
+bst=Breadthfirstsearch()
+bst.addedge(0,1)
+bst.addedge(0,2)
+bst.addedge(0,3)
+bst.addedge(1,4)
+bst.addedge(1,5)
+bst.addedge(2,6)
+bst.addedge(2,7)
+bst.addedge(3,7)
+bst.addedge(4,None)
+bst.addedge(5,None)
+bst.addedge(6,None)
+bst.addedge(7,None)
+print("\nPrinting Graphs Vertex and Edges:")
+bst.printGraph()
+print("\nPrinting Graphs in BreadFirstSearch order :")
+print(bst.search(0))
+print("\n")
+
+bst=Breadthfirstsearch()
+bst.addedge(0,1)
+bst.addedge(0,3)
+bst.addedge(0,8)
+bst.addedge(1,0)
+bst.addedge(1,7)
+bst.addedge(7,1)
+bst.addedge(7,2)
+bst.addedge(2,3)
+bst.addedge(2,5)
+bst.addedge(2,7)
+bst.addedge(3,0)
+bst.addedge(3,2)
+bst.addedge(3,4)
+bst.addedge(4,3)
+bst.addedge(4,8)
+bst.addedge(5,2)
+bst.addedge(5,6)
+bst.addedge(8,0)
+bst.addedge(8,4)
+bst.addedge(6,5)
+print("\nPrinting Graphs Vertex and Edges:")
+bst.printGraph()
+print("\nPrinting Graphs in BreadFirstSearch order :")
+print(bst.search(0))
