d0edce4c5b296ef332025264b84d2e70504ee172
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2020-07-28 02:25:45 +0530

    Create memoization.py

---
+++ Python/DynamicProgramming/memoization.py
@@ -0,0 +1,45 @@
+'''
+Created on 27-Jul-2020
+
+@author: dibyajyoti
+
+Illustration of Dynamic Programming memoization concept of caching.
+'''
+
+calculation1=0
+def fibo1(number):
+
+    global calculation1
+    calculation1+=1
+    fibo_dict=dict()
+    if number < 0:
+        return False
+    elif(number==1):
+        return 0
+    elif(number==2):
+        return 1
+    else:
+        return fibo1(number-1)+fibo1(number-2)
+
+print(fibo1(30))
+print("calculation we doing without memoization",calculation1)
+
+calculation2=0
+memoir=dict()
+def fibo(n):
+    global calculation2
+    global memoir
+    calculation2+=1
+    if n in memoir.keys():
+        return memoir[n]
+    else:
+        if (n ==1):
+            return 0
+        elif (n==2):
+            return 1
+        else:
+            memoir[n]=fibo(n-1)+fibo(n-2)
+    return memoir[n]
+
+print(fibo(30))
+print("calculation we doing with memoization",calculation2)
