176940159577640946a39d03c983a251d1758443
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-03-23 21:35:39 -0700

    Refactor binary search variable names for clarity

--- LeetCode/BinarySearch/875. Koko Eating Bananas.py
+++ LeetCode/BinarySearch/875. Koko Eating Bananas.py
@@ -4,23 +4,23 @@ https://leetcode.com/problems/koko-eating-bananas/
 import math
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-
-        left = 1
-        right = max(piles)
+        low = 1
+        high = max(piles)
         ans = -1
 
-        while(left <= right):
-            mid = (left + right) // 2
-            if self.isFeasible(piles,h,mid):
+        while(low <= high):
+            mid = (low+high) // 2
+            if self.feasible_solution(piles,h,mid):
                 ans = mid
-                right = mid - 1
+                high = mid - 1
             else:
-                left = mid + 1
+                low = mid + 1
         return ans
 
-    def isFeasible(self,piles,h,k):
+    def feasible_solution(self,piles,h,k):
         num_hours = 0
 
         for p in piles:
             num_hours += math.ceil(p/k)
+        
         return num_hours <= h
