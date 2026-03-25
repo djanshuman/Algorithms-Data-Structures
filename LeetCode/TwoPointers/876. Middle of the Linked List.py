21704ced6212fc87c83361ce372f17ccc1651aa7
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-03-24 22:58:44 -0700

    Create 876. Middle of the Linked List.py

---
+++ LeetCode/TwoPointers/876. Middle of the Linked List.py
@@ -0,0 +1,16 @@
+'''
+https://leetcode.com/problems/middle-of-the-linked-list/submissions/1958516687/
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
