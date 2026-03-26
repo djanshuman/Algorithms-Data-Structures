7258c9969550837ad3916e775f98cc8d11b50cbc
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-03-25 17:33:48 -0700

    Update 167. Two Sum II - Input Array Is Sorted.py

--- LeetCode/TwoPointers/167. Two Sum II - Input Array Is Sorted.py
+++ LeetCode/TwoPointers/167. Two Sum II - Input Array Is Sorted.py
@@ -1,7 +1,7 @@
 '''
 https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
 '''
-
+''' O(n) preferred approach'''
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         l = 0
@@ -15,4 +15,27 @@ class Solution:
                 r -= 1
             elif sum < target:
                 l += 1
+        return []
+
+
+'''
+Binary  Search O(nlogn)
+'''
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        for i in range(len(numbers)):
+            compliment = target - numbers[i]
+
+            l = i+1
+            r = len(numbers) - 1
+            while (l <= r):
+                mid = (l + r) // 2
+                if numbers[mid] == compliment:
+                    return [i+1,mid+1]
+                elif numbers[mid] < compliment:
+                    l = mid + 1
+                else:
+                    r = mid - 1
         return []
