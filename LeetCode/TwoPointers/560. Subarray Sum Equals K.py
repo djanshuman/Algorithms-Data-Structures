aa4b84d24aca127d38aa92c8cd6bfb8808c385b4
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-03-27 21:26:48 -0700

    Create 560. Subarray Sum Equals K.py

---
+++ LeetCode/TwoPointers/560. Subarray Sum Equals K.py
@@ -0,0 +1,20 @@
+'''
+https://leetcode.com/problems/subarray-sum-equals-k/description/
+'''
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix_map = {0:1}
+        current_sum = 0
+        total_count = 0
+
+        for i, ele in enumerate(nums):
+            current_sum += ele
+
+            needed = current_sum - k
+            if needed in prefix_map:
+                total_count += prefix_map[needed]
+
+
+            prefix_map[current_sum] = prefix_map.get(current_sum,0) + 1
+        return total_count
