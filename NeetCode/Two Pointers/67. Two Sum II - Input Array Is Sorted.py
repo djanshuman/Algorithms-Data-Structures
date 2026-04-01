3d3a005b0885727c1e2b0205611e3025752a5fe1
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-03-31 22:33:23 -0700

    Add twoSum function for sorted two sum problem
    Implement two-pointer approach to find indices of two numbers that add up to the target in a sorted array.

---
+++ NeetCode/Two Pointers/67. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,18 @@
+'''
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/?envType=study-plan-v2&envId=top-interview-150
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+'''
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(numbers) - 1
+
+        while (l <= r):
+            sum = numbers[l] + numbers[r]
+            if sum == target and l != r:
+                return [l+1,r+1]
+            elif sum > target:
+                r -= 1
+            elif sum < target:
+                l += 1
+        return []
