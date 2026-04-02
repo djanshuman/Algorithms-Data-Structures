7eb2bd81907639f8b9ececde1d7f154637ffa38d
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-04-02 00:28:36 -0700

    Create 84. Largest Rectangle in Histogram.py

---
+++ NeetCode/Stack + Monotonically Category/84. Largest Rectangle in Histogram.py
@@ -0,0 +1,23 @@
+'''
+https://leetcode.com/problems/largest-rectangle-in-histogram/
+
+Current:Array/Stack/Monotonic Stack
+Key Idea:Identifying the nearest smaller neighbors to calculate the maximum width for each bar acting as the minimum height.
+'''
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = [] #monotonically increasing heights
+        max_area = 0
+        heights = heights + [0] # adding a fake bar / sentinel element at the end to ensure all bars are calculated.
+        
+
+        for i , n in enumerate(heights):
+            while stack and heights[stack[-1]] > n:
+                height = heights[stack.pop()]
+                width = i if not stack else (i - stack[-1] - 1)
+                max_area = max(max_area, height * width)
+            stack.append(i)
+
+        return max_area
+        
