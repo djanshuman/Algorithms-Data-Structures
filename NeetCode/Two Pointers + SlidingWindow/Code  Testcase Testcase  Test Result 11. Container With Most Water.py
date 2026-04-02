320d486c072cd1bdc9196d8e1a544a8a1f545458
Dibyajyoti Anshuman Nayak <37104549+djanshuman@users.noreply.github.com>
2026-04-01 21:24:54 -0700

    Create   Code  Testcase Testcase  Test Result 11. Container With Most Water.py

---
+++ NeetCode/Two Pointers + SlidingWindow/Code  Testcase Testcase  Test Result 11. Container With Most Water.py
@@ -0,0 +1,21 @@
+'''
+https://leetcode.com/problems/container-with-most-water/description/
+'''
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        if len(height) == 0:
+            return 0
+
+        max_area = 0
+        l = 0
+        r = len(height) - 1
+
+        while l < r:
+            area = (r - l) * min(height[l],height[r])
+            max_area = max(area,max_area)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
